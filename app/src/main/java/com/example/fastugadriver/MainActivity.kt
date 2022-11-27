d6f7e9830c14e3662a4c8462fa6115945a7a9f60
BrunoSilvaa <61585181+BrunoSilvaa@users.noreply.github.com>
2022-11-27 11:36:32 +0000

    TG-30 AT-1 Allow location fragment added

--- app/src/main/java/com/example/fastugadriver/MainActivity.kt
+++ app/src/main/java/com/example/fastugadriver/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.fastugadriver
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.fastugadriver.data.LoginRepository
 import com.example.fastugadriver.databinding.ActivityMainBinding
 import com.example.fastugadriver.databinding.ActivityRegisterBinding
+import com.example.fastugadriver.ui.LocationAuthFragment
 import com.example.fastugadriver.ui.MapsFragment
 import com.example.fastugadriver.ui.OrdersFragment
 import com.example.fastugadriver.ui.ProfileFragment
@@ -20,9 +26,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Set Maps Fragment as initial
-        replaceFragment(MapsFragment())
-        binding.bottomNavigationView.selectedItemId = R.id.bottom_navbar_map
+        // Set default fragment
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            replaceFragment(LocationAuthFragment())
+        }else{
+            replaceFragment(MapsFragment())
+            binding.bottomNavigationView.selectedItemId = R.id.bottom_navbar_map
+        }
+
 
 
         binding.bottomNavigationView.setOnItemSelectedListener {
