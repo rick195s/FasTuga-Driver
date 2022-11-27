d1df9b77443e3e40ffc58cad0f1560215b81df06
BrunoSilvaa <61585181+BrunoSilvaa@users.noreply.github.com>
2022-11-27 22:39:31 +0000

    TG-30 AT-1 Removed bottom navigation in location authorization fragment

--- app/src/main/java/com/example/fastugadriver/MainActivity.kt
+++ app/src/main/java/com/example/fastugadriver/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         if(ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED) {
+            binding.bottomNavigationView.setVisibility(View.GONE);
             replaceFragment(LocationAuthFragment())
         }else{
             replaceFragment(MapsFragment())
