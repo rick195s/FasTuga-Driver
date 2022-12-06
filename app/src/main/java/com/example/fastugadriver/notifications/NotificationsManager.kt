d9324223939728e9ab42d628c9c22e36797d0b33
Ricardo Franco <ricardosf195@gmail.com>
2022-12-06 16:27:26 +0000

    Order cancelled notification created

---
+++ app/src/main/java/com/example/fastugadriver/notifications/NotificationsManager.kt
@@ -0,0 +1,58 @@
+package com.example.fastugadriver.notifications
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.example.fastugadriver.R
+import com.example.fastugadriver.data.pojos.orders.Order
+
+class NotificationsManager (private val context: Context){
+
+    private val channelId :String ="notification_channel"
+
+    private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    init {
+        createNotificationChannel()
+    }
+
+    fun orderCancelledNotification(order: Order) {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_baseline_cancel_24)
+            .setContentTitle("Order ${order.ticket_number} cancelled")
+            .setContentText("Order  ${order.ticket_number} cancelled. Tax fee: ${order.tax_fee}. Location : ${order.delivery_location} ")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        notificationManager.notify(
+            1, notification
+        )
+
+        saveNotification(notification)
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "FasTugaDriver Notifications"
+            val descriptionText = "Used for notifications received from websockets"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun saveNotification(notification : Notification){
+
+    }
+}
